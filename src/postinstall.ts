7346ffefbe41b55e16ff4ab592e9d92957fb18e1
dsampson94 <davesampson15@gmail.com>
2024-07-12 12:04:46 +0200

    fixes post install

--- src/postinstall.ts
+++ src/postinstall.ts
@@ -1,19 +1,12 @@
-import * as path from 'path';
-import { generateContextFile, createDefaultConfig, addToGitignore } from './utils';
-import { CONFIG_FILE_NAME, OUTPUT_FILE_NAME } from './config';
+#!/usr/bin/env ts-node
 
-const rootDir = process.cwd();
-const configFilePath = path.join(rootDir, CONFIG_FILE_NAME);
-const gitignorePath = path.join(rootDir, '.gitignore');
+import { exec } from 'child_process';
 
-/**
- * Main function to run the script. This function checks if the configuration file and output file exist,
- * creates them if they don't, and then adds these files to .gitignore. Finally, it generates the context file based on the configuration.
- */
-const run = (): void => {
-    createDefaultConfig(configFilePath);
-    addToGitignore({ gitignorePath, entries: [CONFIG_FILE_NAME, OUTPUT_FILE_NAME] });
-    generateContextFile();
-};
-
-run();
+// Execute the 'npx contxt' command
+exec('npx contxt', (err, stdout, stderr) => {
+    if (err) {
+        console.error(`Error: ${stderr}`);
+        process.exit(1);
+    }
+    console.log(stdout);
+});
